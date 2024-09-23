a93f162603981379d1f99365957a1055ddfbf9d6
alanzhang03 <107202923+alanzhang03@users.noreply.github.com>
2024-09-23 19:26:25 -0400

    text aligned

---
+++ components/MainPageDisplay.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles/MainPageDisplay.scss";
+
+const MainPageDisplay = () => {
+  return (
+    <div className="main-page-container">
+      <h1>Welcome to the TopFlight Tracker</h1>
+      <p>
+        Explore detailed standings, match history, lineups, and more from the
+        Premier League, La Liga, Bundesliga, and Champions League.
+      </p>
+    </div>
+  );
+};
+
+export default MainPageDisplay;
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,11 @@
 import Image from "next/image";
-import "./page.module.css";
 import Link from "next/link";
+import MainPageDisplay from "../../components/MainPageDisplay";
 
 export default function Home() {
   return (
     <>
-      <div className="main-page-container">
-        <h1>Welcome to the TopFlight Tracker</h1>
-        <p>
-          Explore detailed standings, match history, lineups, and more from the
-          Premier League, La Liga, Bundesliga, and Champions League.
-        </p>
-      </div>
+      <MainPageDisplay />
     </>
   );
 }
