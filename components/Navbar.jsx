d3cc199d144e10a07cb226b14b4fa409d74e9746
alanzhang03 <107202923+alanzhang03@users.noreply.github.com>
2025-03-31 11:10:23 -0400

    updated info

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,70 +9,70 @@ import { IoCalendarOutline } from "react-icons/io5";
 import { IoMedalOutline } from "react-icons/io5";
 
 export default function Navbar({ league }) {
-	const leagueUrls = {
-		bundesliga: {
-			name: "Bundesliga",
-			standings: "/leagues/bundesliga/standings",
-			fixtures: "/leagues/bundesliga/fixtures",
-			results: "/leagues/bundesliga/results",
-		},
-		epl: {
-			name: "Premier League",
-			standings: "/leagues/premier-league/standings",
-			fixtures: "/leagues/premier-league/fixtures",
-			results: "/leagues/premier-league/results",
-		},
-		championsleague: {
-			name: "Champions League",
-			standings: "/leagues/champions-league/standings",
-			fixtures: "/leagues/champions-league/fixtures",
-			results: "/leagues/champions-league/results",
-		},
-		laliga: {
-			name: "La Liga",
-			standings: "/leagues/la-liga/standings",
-			fixtures: "/leagues/la-liga/fixtures",
-			results: "/leagues/la-liga/results",
-		},
-	};
+  const leagueUrls = {
+    epl: {
+      name: "Premier League",
+      standings: "/leagues/premier-league/standings",
+      fixtures: "/leagues/premier-league/fixtures",
+      results: "/leagues/premier-league/results",
+    },
+    laliga: {
+      name: "La Liga",
+      standings: "/leagues/la-liga/standings",
+      fixtures: "/leagues/la-liga/fixtures",
+      results: "/leagues/la-liga/results",
+    },
+    championsleague: {
+      name: "Champions League",
+      standings: "/leagues/champions-league/standings",
+      fixtures: "/leagues/champions-league/fixtures",
+      results: "/leagues/champions-league/results",
+    },
+    bundesliga: {
+      name: "Bundesliga",
+      standings: "/leagues/bundesliga/standings",
+      fixtures: "/leagues/bundesliga/fixtures",
+      results: "/leagues/bundesliga/results",
+    },
+  };
 
-	const currentLeague = leagueUrls[league];
-	const pathname = usePathname();
+  const currentLeague = leagueUrls[league];
+  const pathname = usePathname();
 
-	if (!currentLeague) {
-		return <div>League not found</div>;
-	}
+  if (!currentLeague) {
+    return <div>League not found</div>;
+  }
 
-	return (
-		<nav className="navbar-prop">
-			<div className="navbar-container-prop">
-				<ul className="navbar-menu-prop">
-					<li className={pathname === currentLeague.standings ? "active" : ""}>
-						<Link href={currentLeague.standings}>
-							<div className="results-navbar-container">
-								<MdOutlineLeaderboard />
-								Standings
-							</div>
-						</Link>
-					</li>
-					<li className={pathname === currentLeague.fixtures ? "active" : ""}>
-						<Link href={currentLeague.fixtures}>
-							<div className="results-navbar-container">
-								<IoCalendarOutline />
-								Fixtures
-							</div>
-						</Link>
-					</li>
-					<li className={pathname === currentLeague.results ? "active" : ""}>
-						<Link href={currentLeague.results}>
-							<div className="results-navbar-container">
-								<IoMedalOutline />
-								Results
-							</div>
-						</Link>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar-prop">
+      <div className="navbar-container-prop">
+        <ul className="navbar-menu-prop">
+          <li className={pathname === currentLeague.standings ? "active" : ""}>
+            <Link href={currentLeague.standings}>
+              <div className="results-navbar-container">
+                <MdOutlineLeaderboard />
+                Standings
+              </div>
+            </Link>
+          </li>
+          <li className={pathname === currentLeague.fixtures ? "active" : ""}>
+            <Link href={currentLeague.fixtures}>
+              <div className="results-navbar-container">
+                <IoCalendarOutline />
+                Fixtures
+              </div>
+            </Link>
+          </li>
+          <li className={pathname === currentLeague.results ? "active" : ""}>
+            <Link href={currentLeague.results}>
+              <div className="results-navbar-container">
+                <IoMedalOutline />
+                Results
+              </div>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
--- src/app/globals/NavbarMain.jsx
+++ src/app/globals/NavbarMain.jsx
@@ -4,35 +4,32 @@ import Link from "next/link";
 import "./NavbarMain.scss";
 
 const NavbarMain = () => {
-	return (
-		<nav className="navbar">
-			<div className="navbar-container">
-				<ul className="navbar-menu">
-					<li>
-						<Link href="/">Home</Link>
-					</li>
-					<li>
-						<Link href="/favorites">Favorites</Link>
-					</li>
-
-					<li>
-						<Link href="/leagues/bundesliga">Bundesliga</Link>
-					</li>
-					<li>
-						<Link href="/leagues/premier-league">Premier League</Link>
-					</li>
-
-					<li>
-						<Link href="/leagues/champions-league">Champions League</Link>
-					</li>
-
-					<li>
-						<Link href="/leagues/la-liga">La Liga</Link>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <ul className="navbar-menu">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/favorites">Favorites</Link>
+          </li>
+          <li>
+            <Link href="/leagues/premier-league">Premier League</Link>
+          </li>
+          <li>
+            <Link href="/leagues/la-liga">La Liga</Link>
+          </li>
+          <li>
+            <Link href="/leagues/champions-league">Champions League</Link>
+          </li>
+          <li>
+            <Link href="/leagues/bundesliga">Bundesliga</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default NavbarMain;
