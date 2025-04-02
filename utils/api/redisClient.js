e546e3933dfd2c29548b4f1f8fec8f131bb53e2a
alanzhang03 <107202923+alanzhang03@users.noreply.github.com>
2025-04-01 22:34:19 -0400

    redis fixed

--- utils/api/redisClient.js
+++ utils/api/redisClient.js
@@ -2,20 +2,20 @@ import { createClient } from "redis";
 
 let redisClient;
 
-const getRedisClient = async () => {
-	if (!redisClient) {
-		redisClient = createClient({
-			url: process.env.REDIS_URL,
-		});
+if (!global.redisClient) {
+  global.redisClient = createClient({
+    url: process.env.REDIS_URL,
+  });
+  global.redisClient.on("error", (err) => {
+    console.error("Redis Client Error", err);
+  });
+  global.redisClient.connect().catch((err) => {
+    console.error("Redis Connect Error", err);
+  });
+}
 
-		redisClient.on("error", (err) => {
-			console.error("Redis Client Error", err);
-		});
+redisClient = global.redisClient;
 
-		await redisClient.connect();
-	}
-
-	return redisClient;
-};
+const getRedisClient = async () => redisClient;
 
 export default getRedisClient;
