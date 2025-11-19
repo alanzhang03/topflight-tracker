c8bf447dc2ed4d35eb9999e88899027523e52322
alanzhang03 <107202923+alanzhang03@users.noreply.github.com>
2025-11-19 17:08:15 -0500

    updated env, info

--- src/app/context/FavoritesContext.js
+++ src/app/context/FavoritesContext.js
@@ -4,29 +4,28 @@ import React, { createContext, useContext, useState, useEffect } from "react";
 const FavoritesContext = createContext();
 
 export const FavoritesProvider = ({ children }) => {
-	const [favorites, setFavorites] = useState([]);
-	useEffect(() => {
-		const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
-		setFavorites(storedFavorites);
-	}, []);
+  const [favorites, setFavorites] = useState([]);
+  useEffect(() => {
+    const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    setFavorites(storedFavorites);
+  }, []);
 
-	useEffect(() => {
-		localStorage.setItem("favorites", JSON.stringify(favorites));
-	}, [favorites]);
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
 
-	const toggleFavorite = (item) => {
-		setFavorites((prevFavorites) =>
-			prevFavorites.some((fav) => fav.id === item.id)
-				? prevFavorites.filter((fav) => fav.id !== item.id)
-				: [...prevFavorites, item]
-		);
-	};
-
-	return (
-		<FavoritesContext.Provider value={{ favorites, toggleFavorite }}>
-			{children}
-		</FavoritesContext.Provider>
-	);
+  const toggleFavorite = (item) => {
+    setFavorites((prevFavorites) =>
+      prevFavorites.some((fav) => fav.id === item.id)
+        ? prevFavorites.filter((fav) => fav.id !== item.id)
+        : [...prevFavorites, item]
+    );
+  };
+  return (
+    <FavoritesContext.Provider value={{ favorites, toggleFavorite }}>
+      {children}
+    </FavoritesContext.Provider>
+  );
 };
 
 export const useFavorites = () => useContext(FavoritesContext);
